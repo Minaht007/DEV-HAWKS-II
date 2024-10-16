7630b928ad5173af767eeb31121759212d887ea0
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-10-16 11:00:47 +0300

    123

--- src/components/consServComponent.jsx
+++ src/components/consServComponent.jsx
@@ -1,4 +1,4 @@
-import { ContactForm } from "@/components/contactForm";
+import  ContactForm  from "@/components/contactForm";
 
 const title = "text-4xl font-bold py-20 px-10 text-textContacts";
 
@@ -8,7 +8,7 @@ const CsServiceComponent = () => {
       <h1 className={title} style={{ textAlign: "justify" }}>
         Leave your question and contacts and we are answer in nearest time&#128512;
       </h1>
-      {ContactForm}
+      <ContactForm/>
     </div>
   );
 };
