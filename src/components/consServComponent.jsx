2839618e930069e1998b4cd21810864d9e103151
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-10-18 11:57:05 +0300

    12345

--- src/components/consServComponent.jsx
+++ src/components/consServComponent.jsx
@@ -1,13 +1,17 @@
 import  ContactForm  from "@/components/contactForm";
+import { useTranslations } from "next-intl";
 
 const title = "sm:text-xl md:text-2xl  desk:text-4xl font-bold py-20 px-10 text-textContacts";
 
 const CsServiceComponent = () => {
+  const t = useTranslations('ContactUs')
+
+
   return (
     <div className="flex flex-col items-center justify-center py-20 layout">      
       <ContactForm/>
       <h1 className={title} style={{ textAlign: "justify" }}>
-        Leave your question and contacts and we are answer in nearest time&#128512;
+        {t('slogan')}&#128512;
       </h1>
     </div>
   );
