ad4e04ace36feea6a3c109a20dac31f002790dc6
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-10-22 16:10:35 +0300

    layout

--- src/components/advantages.jsx
+++ src/components/advantages.jsx
@@ -2,7 +2,7 @@ import { useTranslations } from "next-intl";
 
 const svgStyle = "sm:w-6 sm:h-6 md:w-8 md:h-8 desk:w-10 desk:h-10 md:mx-2 md:my-2";
 const textStyle = "md:text-lg desk:text-xl font-bold px-2 md:text-start";
-const divStyle = "flex flex-col items-center justify-between w-full desk:w-auto"; // Изменено на auto для desk
+const divStyle = "flex flex-col items-center justify-between w-full desk:w-auto";
 
 const Advantage = () => {
   const t = useTranslations('ADV');
