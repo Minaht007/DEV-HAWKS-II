6de906ca1fb1f86c0aeffd53218cc7544e974601
Andrey <108063187+Minaht007@users.noreply.github.com>
2024-10-30 10:00:02 +0200

    mobile Price

--- src/components/pricesTabMobile.jsx
+++ src/components/pricesTabMobile.jsx
@@ -1,61 +1,94 @@
 import { useTranslations } from "next-intl";
 
+const titleStyle = "text-clamp text-textContacts mb-10 font-bold";
+const underTitle = "text-clamp text-textContacts mb-10";
+
 const PricesTabForMob = () => {
   const t = useTranslations("Prices");
 
   return (
-    <div className="max-w-[767px]">
-      <table className="flex flex-col items-center justify-center my-10">
+    <div className="flex flex-col items-center min-w-[390px] max-w-[767px] px-10 my-10">
+      <h1 className={titleStyle}>{t("title")}</h1>
+      <p className={underTitle}>{t("underTitleOne")}</p>
+      <p className={underTitle}>{t("underTitleTwo")}</p>
+
+      <table className="flex flex-col items-center my-10">
         <caption className="w-[400px]">
-          <h2 className="text-xl text-textContacts">{t("tableCaption")}</h2>
+          <h2 className="text-xl text-textContacts underline">{t("tableCaption")}</h2>
         </caption>
 
         <thead className="flex justify-around min-w-[400px] max-w-[600px]">
           <tr>
-            <th className="pr-20 text-center text-textContacts text-[clamp(1rem, 2vw + 1rem, 3rem)]">{t("tabTitleOne")}</th>
-            <th className="text-textContacts text-[clamp(1rem, 2vw + 1rem, 3rem)]" >{t("tabTitleTwo")}</th>
+            <th className="pr-20 text-center text-textContacts text-clamp">
+              {t("tabTitleOne")}
+            </th>
+            <th className="text-textContacts text-clamp">{t("tabTitleTwo")}</th>
           </tr>
         </thead>
 
         <tbody className="flex justify-around">
-          <tr className="flex flex-col  border-textContacts border-t-2 border-b-2 bg-numberBG rounded-[16px] mx-6 ">
-            <td className="border-textContacts border-r-2 px-4 mt-4 text-center text-secondaryTextColor" >{t("typeApplicationLanding")}</td>
-            <td className="border-textContacts border-r-2 px-4 py-2 text-center text-secondaryTextColor">{t("typeApplicationSmall")}</td>
-            <td className="border-textContacts border-r-2 px-4 pb-2 text-center text-secondaryTextColor">{t("typeApplicationMiddle")}</td>
-            <td className="border-textContacts border-r-2 px-4 mb-4 text-center text-secondaryTextColor">{t("typeApplicationLarge")}</td>            
+          <tr className="flex flex-col  border-textContacts border-t-2 border-b-2 bg-numberBG rounded-[16px] mr-6 ">
+            <td className="border-textContacts border-r-2 padding-clamp mt-4 text-center text-secondaryTextColor">
+              {t("typeApplicationLanding")}
+            </td>
+            <td className="border-textContacts border-r-2 padding-clamp py-2 text-center text-secondaryTextColor">
+              {t("typeApplicationSmall")}
+            </td>
+            <td className="border-textContacts border-r-2 padding-clamp pb-2 text-center text-secondaryTextColor">
+              {t("typeApplicationMiddle")}
+            </td>
+            <td className="border-textContacts border-r-2 padding-clamp mb-4 text-center text-secondaryTextColor">
+              {t("typeApplicationLarge")}
+            </td>
           </tr>
           <tr className="flex flex-col items-center justify-center border-textContacts border-t-2 border-b-2 bg-numberBG rounded-[16px]">
-            <td className="text-center mx-auto min-w-[100px] max-w-[150px] text-secondaryTextColor">{t("pricesLanding")}</td>
-            <td className="text-center mx-auto min-w-[100px] py-2 max-w-[150px] text-secondaryTextColor">{t("pricesSmall")}</td>
-            <td className="text-center mx-auto min-w-[100px] pb-2 max-w-[150px] text-secondaryTextColor">{t("pricesMiddle")}</td>
-            <td className="text-center mx-auto min-w-[100px] max-w-[150px] text-secondaryTextColor">{t("pricesLarge")}</td>            
+            <td className="text-center padding-clamp mx-auto  text-secondaryTextColor">
+              {t("pricesLanding")}
+            </td>
+            <td className="text-center  padding-clamp py-2 text-secondaryTextColor">
+              {t("pricesSmall")}
+            </td>
+            <td className="text-center  padding-clamp pb-2 text-secondaryTextColor">
+              {t("pricesMiddle")}
+            </td>
+            <td className="text-center  padding-clamp text-secondaryTextColor">
+              {t("pricesLarge")}
+            </td>
           </tr>
         </tbody>
+      </table>
+
+      <table className="mx-5">
+        <caption className="w-[400px]">
+            <h2 className="text-clamp text-textContacts underline">{t('rentTitle')}</h2>
+        </caption>
+
+        <thead className="flex justify-around min-w-[400px] max-w-[600px]">
+          <tr>
+            <th className="pr-20 text-center text-textContacts text-clamp">
+              {t("tabTitleOne")}
+            </th>
+            <th className="text-textContacts text-clamp">{t("tabTitleTwo")}</th>
+          </tr>
+        </thead>
+
+        <tbody className="flex flex-col justify-around ">
+            <tr className="flex justify-center items-center bg-numberBG rounded-[16px] mb-4 py-2">
+                <td className="w-3/5 px-2 mx-auto text-secondaryTextColor">{t('rentLanding')}</td>
+                <td className="flex justify-center w-2/5 px-2 text-secondaryTextColor">{t('priceRentLanding')}</td>                
+            </tr>
+
+            <tr className="flex justify-center items-center bg-numberBG rounded-[16px] py-2">
+                <td className="w-3/5 px-2 mx-auto text-secondaryTextColor">{t('rentLargeBusiness')}</td>
+                <td className="flex justify-center w-2/5 px-2 text-secondaryTextColor">{t('priceRentLarge')}</td>
+            </tr>
+        </tbody>
+
+
       </table>
     </div>
   );
 };
 export default PricesTabForMob;
 
-{
-  /* <div className="flex mx-4 min-w-[400px] max-w-[700px]">
-<div className="w-2/3">
-    <p>{t('typeApplicationLanding')} <br/> {t('leadTimeLanding')}</p>
-    <p>{t('typeApplicationSmall')} <br/> {t('leadTimeLanding')}</p>
-    <p>{t('typeApplicationMiddle')} <br/> {t('leadTimeLanding')}</p>
-    <p>{t('typeApplicationLarge')} <br/> {t('leadTimeLanding')}</p>
-</div>
 
-<div className="1/3 px-4">
-    <p>{t('pricesLanding')}</p>
-    <p className="my-3"></p>
-    <p>{t('pricesSmall')}</p>
-    <p className="my-3"></p>
-    <p>{t('pricesMiddle')}</p>
-    <p className="my-3"></p>
-    <p>{t('pricesLarge')}</p>
-</div>
-
-
-</div> */
-}
